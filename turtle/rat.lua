a29a8d24c89f6c09c350cf66046504a4d01f5a0b
Vulcalien <vulcalien@gmail.com>
2021-09-16 16:12:27 +0200

    rat.lua: fix turtle not calculating the fuel correctly

--- turtle/rat.lua
+++ turtle/rat.lua
@@ -1,5 +1,7 @@
 -- Copyright 2021 Vulcalien
--- 2021-09-15 20:20 +0200
+--
+-- rat.lua - 1.1
+-- 2021-09-16 15:30 +0200
 
 local tArgs = { ... }
 if #tArgs < 1 then
@@ -21,28 +23,39 @@ function tryRefuel()
     return false
 end
 
+function isInvFull()
+    for i=1,16 do
+        if turtle.getItemCount(i)
+           == 0 then
+            return false
+        end
+    end
+
+    return true
+end
+
 function goHome()
     print('Going home.')
 
     turtle.turnLeft()
     turtle.turnLeft()
-    
+
     -- go back home
     for _=1,traveled - 1 do
         turtle.forward()
         turtle.dig()
     end
     turtle.forward()
-    
+
     -- clear inventory
     for i=1,16 do
         turtle.select(i)
         turtle.drop()
     end
-    
+
     turtle.turnRight()
     turtle.turnRight()
-    
+
     traveled = 0
 end
 
@@ -51,32 +64,21 @@ local maxTravel = tonumber(tArgs[1])
 while traveled < maxTravel do
     -- check fuel level
     local fuel = turtle.getFuelLevel()
-      -- the turtle has to go forward and then
-      -- back, so fuel must be even
-    fuel = fuel - fuel % 2
-    if fuel <= traveled then
+    if fuel <= traveled + 1 then
         if not tryRefuel() then
             print('Out of fuel.')
             goHome()
             return
         end
     end
-    
+
     -- dig next block
     turtle.dig()
     turtle.forward()
     traveled = traveled + 1
-    
+
     -- check inventory space
-    local invFull = true
-    for i=1,16 do
-        if turtle.getItemCount(i)
-           == 0 then
-            invFull = false
-            break
-        end
-    end
-    if invFull then
+    if isInvFull() then
         print('Inventory is full.')
         goHome()
     end
